741f985352fb1df3188aa506b444578d95ac879f
Mohamed Safwan <safwan95@gmail.com>
2016-07-10 06:14:30 +0530

    linked the two forms to the DB

--- rangde/features/models.py
+++ rangde/features/models.py
@@ -19,5 +19,12 @@ class seek_info_table(models.Model):
     class Meta:
         db_table = "seek_info"
         
+class refer_info_table(models.Model):
+    referrer_name = models.CharField(max_length=255, null=True, blank=True)
+    referred_name = models.CharField(max_length=255, null=True, blank=True)
+    email_id = models.CharField(max_length=255, null=True, blank=True)
+    mobile_number = models.CharField(max_length=255,null=True, blank=True)
+            
 
-    
+    class Meta:
+        db_table = "refer_info"
--- rangde/home/views.py
+++ rangde/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
-from features.models import seek_info_table
+from django.http.response import HttpResponse, HttpResponseRedirect
+from features.models import seek_info_table, refer_info_table
 
 def home_page(request):
     return render(request, "index.html", {})
@@ -11,5 +11,25 @@ def seek_info(request):
         email = request.POST.get('email')
         mobile= request.POST.get('mobile')
         question = request.POST.get('question')
+        refer_name = None
         
-        return HttpResponse("hello")
+        seek_info_table.objects.create(visitor_name=visitor_name,
+                                       refer_name=refer_name,
+                                       mobile_number=mobile,
+                                       email_id=email)
+        
+    return HttpResponseRedirect("/home/")
+
+def refer_info(request):
+    if request.POST:
+        referrer_name = request.POST.get('referrer_name')
+        referred_name = request.POST.get('referred_name')
+        email_id = request.POST.get('email')
+        mobile_number = request.POST.get('mobile')
+        
+        refer_info_table.objects.create(referrer_name=referrer_name,
+                                        referred_name=referred_name,
+                                        email_id=email_id,
+                                        mobile_number=mobile_number,
+                                        )
+    return HttpResponseRedirect("/home/")
